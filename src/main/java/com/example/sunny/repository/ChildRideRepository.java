930804a624000f2c77f8f1e978e303c801659dec
bowwow2115 <bowwow2115@gmail.com>
2024-06-30 12:01:24 +0900

    ChildRide -> SunnyRide 이름 변경

--- src/main/java/com/example/sunny/repository/ChildRideRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sunny.repository;
-
-import com.example.sunny.model.ChildRide;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChildRideRepository extends JpaRepository<ChildRide, Long> {
-}
