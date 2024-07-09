c6f5cd15a939c680545e6ecafb2b73f3f42f736a
SeungHunPark <bowwow94@inzent.com>
2024-07-09 19:05:27 +0900

    부모, 차량정보 추가, 변경기능 추가

--- src/main/java/com/example/sunny/controller/ParentsController.java
+++ src/main/java/com/example/sunny/controller/ParentsController.java
@@ -5,10 +5,7 @@ import com.example.sunny.service.ParentsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -19,6 +16,10 @@ import java.util.Map;
 public class ParentsController extends BasicController {
     private final ParentsService parentsService;
 
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> addParents(@RequestBody ParentsDto parentsDto) {
+        return createResponse(parentsService.create(parentsDto));
+    }
     @PutMapping
     public ResponseEntity<Map<String, Object>> updateParents(@RequestBody ParentsDto parentsDto) {
         return createResponse(parentsService.update(parentsDto));
