af5562be46479a7305c945035e9ffbc7f836fd30
bowwow2115 <bowwow2115@gmail.com>
2024-07-14 14:43:07 +0900

    path 설정 변경

--- src/main/java/com/example/sunny/config/SecurityConfig.java
+++ src/main/java/com/example/sunny/config/SecurityConfig.java
@@ -4,18 +4,15 @@ import com.example.sunny.config.jwt.JwtAuthenticationEntryPoint;
 import com.example.sunny.config.jwt.JwtRequestFilter;
 import com.example.sunny.service.AuthUserDetailsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -67,11 +64,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/login/**",
                         "/logout/**",
                         "/user",
-                        "/app/js/**",
-                        "/app/css/**",
-                        "/app/img/**",
-                        "/app/fonts/**",
-                        "/test/**"
+                        "/app/**"
+//                        "/app/js/**",
+//                        "/app/css/**",
+//                        "/app/img/**",
+//                        "/app/fonts/**"
                 ).permitAll()
 				.anyRequest().authenticated()
                 .and()
