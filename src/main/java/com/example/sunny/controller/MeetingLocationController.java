77cbdae1073b5f4f58f41ea60651d7a379b8dbc9
bowwow2115 <bowwow2115@gmail.com>
2024-08-18 16:49:19 +0900

    원아삭제, 승하차장소 추가삭제 추가

--- src/main/java/com/example/sunny/controller/MeetingLocationController.java
+++ src/main/java/com/example/sunny/controller/MeetingLocationController.java
@@ -4,20 +4,27 @@ import com.example.sunny.model.dto.MeetingLocationDto;
 import com.example.sunny.service.MeetingLoactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/meetingLocation")
 @RequiredArgsConstructor
-public class MeetingLoactionController extends BasicController{
+public class MeetingLocationController extends BasicController{
     private final MeetingLoactionService meetingLoactionService;
     @PutMapping
     public ResponseEntity<Map<String, Object>> updateMeetingLoaction(@RequestBody MeetingLocationDto meetingLocation) {
        return createResponse(meetingLoactionService.update(meetingLocation));
     }
-}
+
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> addMeetingLoaction(@RequestBody MeetingLocationDto meetingLocation) {
+        return createResponse(meetingLoactionService.create(meetingLocation));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Map<String, Object>> deleteMeetingLoaction(@RequestParam Long id) {
+        meetingLoactionService.deleteById(id);
+        return createResponse();
+    }}
